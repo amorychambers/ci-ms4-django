1db9d1214222020bdd91f93ec3e1f675143f59df
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-10 12:02:29 +0100

    feat: add js file header-toggle.js to base template for toggling which collapsible is visible between navbar and search

---
+++ static/js/header-toggle.js
@@ -0,0 +1,17 @@
+let searchButton = document.getElementById("search-icon");
+let navButton = document.getElementById("navbar-icon");
+let links = document.getElementById("links");
+let searchBox = document.getElementById("searchbox");
+
+
+$(searchButton).click(function() {
+    if ($(links).hasClass("show")) {
+        $(navButton).click()
+    };
+});
+
+$(navButton).click(function() {
+    if ($(searchBox).hasClass("show")) {
+        $(searchButton).click()
+    };
+});
