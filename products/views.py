acf678a40464b08e03f45cfd30a01605325a7982
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-14 19:30:14 +0100

    feat: add product filtering to products.views.py

--- products/views.py
+++ products/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
 
 def products(request):
+    """
+    View to display and sort all products
+    """
 
     products = Product.objects.all()
+    sort = None
+
+    if request.GET:
+        sort = request.GET['sort']
+        products = Product.objects.filter(Q(tags__icontains=sort))
 
     context = {
         "products": products,
+        "sort": sort,
     }
     
     return render(request, 'products/products.html', context)
