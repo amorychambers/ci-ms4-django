228b930880cd2816d96a20b4d78894f347ab3c6b
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-17 20:19:29 +0100

    feat: style disabled buttons for quantity selector

---
+++ static/js/quantity-input.js
@@ -0,0 +1,44 @@
+// Code snippet from Code Institute Boutique Ado django project
+// Script to handle the buttons to increment and decrement the quantity of an item being added to the shopping cart
+
+// Disable +/- buttons outside 1-99 range
+function handleEnableDisable(itemId) {
+    var currentValue = parseInt($(`#id_qty_${itemId}`).val());
+    var minusDisabled = currentValue < 2;
+    var plusDisabled = currentValue > 98;
+    $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
+    $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
+}
+
+// Ensure proper enabling/disabling of all inputs on page load
+var allQtyInputs = $('.qty_input');
+for(var i = 0; i < allQtyInputs.length; i++){
+    var itemId = $(allQtyInputs[i]).data('item_id');
+    handleEnableDisable(itemId);
+}
+
+// Check enable/disable every time the input is changed
+$('.qty_input').change(function() {
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
+});
+
+// Increment quantity
+$('.increment-qty').click(function(e) {
+   e.preventDefault();
+   var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+   var currentValue = parseInt($(closestInput).val());
+   $(closestInput).val(currentValue + 1);
+   var itemId = $(this).data('item_id');
+   handleEnableDisable(itemId);
+});
+
+// Decrement quantity
+$('.decrement-qty').click(function(e) {
+   e.preventDefault();
+   var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+   var currentValue = parseInt($(closestInput).val());
+   $(closestInput).val(currentValue - 1);
+   var itemId = $(this).data('item_id');
+   handleEnableDisable(itemId);
+});
