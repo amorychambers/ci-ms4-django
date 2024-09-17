a433d9ccabf1df8fd6b74bd2e7811fcb37ea9703
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-17 21:56:25 +0100

    feat: add update and remove item links to cart.html

---
+++ static/js/update-cart.js
@@ -0,0 +1,22 @@
+// Code snippet from Code Institute Boutique Ado django project
+// Script to handle updating quantities from the shopping cart page
+
+
+// Update quantity on click
+$('.update-link').click(function (e) {
+    var form = $(this).prev('.update-form');
+    form.submit();
+})
+
+// Remove item and reload on click
+$('.remove-item').click(function (e) {
+    var csrfToken = "{{ csrf_token }}";
+    var itemId = $(this).attr('id').split('remove_')[1];
+    var url = `/bag/remove/${itemId}`;
+    var data = { 'csrfmiddlewaretoken': csrfToken };
+
+    $.post(url, data)
+        .done(function () {
+            location.reload();
+        });
+})
