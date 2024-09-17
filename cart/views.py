ae7aa79de88920f3b533967fa21d985c1ce3cfd1
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-17 16:52:03 +0100

    feat: create view in cart.views.py for adding items to cart and storing in session

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -8,3 +8,22 @@ def cart(request):
     """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, product_id):
+    """
+    View to add a new item to the shopping cart
+    """
+
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
+
+    cart = request.session.get("cart", {})
+
+    if product_id in list(cart.keys()):
+        cart[product_id] += quantity
+    else:
+        cart[product_id] = quantity
+
+    request.session["cart"] = cart
+
+    return redirect(redirect_url)
