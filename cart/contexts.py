72a4d7b62ed33a8e563890336f5f8c47d4260ae1
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-17 17:10:37 +0100

    feat: add cart_products from session to cart context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -3,16 +3,33 @@ Context processor for shopping cart items
 """
 
 from django.conf import settings
-from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
 
-    cart_items = []
+    cart_products = []
     total = 0
     product_count = 0
 
+    cart = request.session.get("cart", {})
+
+    for product_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=product_id)
+        if product.sale:
+            total += (product.price * product.sale) * quantity
+        else:
+            total += product.price * quantity
+        product_count += quantity
+
+        cart_products.append({
+            "product_id": product.id,
+            "quantity": quantity,
+            "product": product
+        })
+
     context = {
-        "cart_items": cart_items,
+        "cart_products": cart_products,
         "total": total,
         "product_count": product_count
     }
