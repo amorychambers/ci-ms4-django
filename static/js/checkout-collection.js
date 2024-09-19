d685dad23f2a76d887b758f76b42f3c33e4a22c4
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-19 19:09:05 +0100

    feat: add checkout-collection.js file to toggle delivery options

--- static/js/checkout-collection.js
+++ static/js/checkout-collection.js
@@ -1,6 +1,9 @@
 collectionPopover = `<i class="fa-regular fa-circle-question" data-bs-toggle="popover" data-bs-content="If you are in our area, you can come pick up your order from the roasters and skip the delivery!"></i>`
-
 $(collectionPopover).insertAfter($('#id_collection'))
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+$('#id_collection').on('click', function() {
+    $('#delivery-fieldset').toggle();
+})
