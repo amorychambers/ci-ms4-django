c7023fa1379f002b41cdaed00306081e986ec4d8
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-20 15:29:17 +0100

    feat: add form submit handler to stripe-elements.js

--- static/js/stripe-elements.js
+++ static/js/stripe-elements.js
@@ -6,11 +6,11 @@
     https://stripe.com/docs/stripe-js
 */
 
-const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-const client_secret = $('#id_client_secret').text().slice(1, -1);
-const stripe = Stripe(stripe_public_key);
+const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+const clientSecret = $('#id_client_secret').text().slice(1, -1);
+const stripe = Stripe(stripePublicKey);
 const elements = stripe.elements();
-const payment_style = {
+const paymentStyle = {
     base: {
         color: '#000',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
@@ -26,8 +26,9 @@ const payment_style = {
     }
 };
 const options = {
-    style: payment_style,
+    style: paymentStyle,
   };
+
 const card = elements.create('card', options);
 card.mount('#card-element');
 
@@ -45,4 +46,34 @@ card.addEventListener('change', function (event) {
     } else {
         errorDiv.textContent = '';
     }
+});
+
+// Handle form submit
+let form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            let errorDiv = document.getElementById('card-errors');
+            let html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
