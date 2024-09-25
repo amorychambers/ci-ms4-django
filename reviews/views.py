6f16dcba3e9f5c5f8fe95d53471a5533260cf6b1
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-25 17:51:13 +0100

    feat: create view to handle new review post requests

--- reviews/views.py
+++ reviews/views.py
@@ -1,28 +1,56 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from products.models import Product
+from .models import Review
 from .forms import ReviewForm
 
 # Create your views here.
 
+@login_required
 def review(request, product_id):
     """
     View to create new reviews
     """
-    if request.user.is_authenticated:
-        user = get_object_or_404(User, username=request.user)
-        product = get_object_or_404(Product, id=product_id)
+    user = get_object_or_404(User, username=request.user)
+    product = get_object_or_404(Product, id=product_id)
+    try:
+        review = Review.objects.get(
+            user = user,
+            review_product = product
+        )
+        form = ReviewForm(instance=review)
+    except Review.DoesNotExist:
+        review = Review.objects.create(
+            user=user,
+            review_product=product
+        )
         form = ReviewForm()
 
-        context = {
-            'user': user,
-            'product': product,
-            'form': form,
-            'review': True
-        }
-        return render(request, 'reviews/review.html', context)
-    else:
-        messages.error(request, "Sorry! You can only review \
-                       products while signed in.")
-        return redirect(reverse('product_details/{product_id}'))
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid() and int(
+            request.POST.get('user')) == request.user.id:
+            form.save()
+            messages.success(request, 'Thanks for leaving a review! \
+                              We appreciate all feedback.')
+            return redirect(reverse('product_details',
+                                     kwargs={'product_id':product_id}))
+        else:
+            messages.error(request, 'Something went wrong! Please check \
+                           that you completed both parts of the review.')
+            return redirect(reverse('review', kwargs={'product_id':product_id}))
+
+    context = {
+        'user': user,
+        'product': product,
+        'form': form,
+    }
+    return render(request, 'reviews/review.html', context)
+
+
+    # else:
+    #     messages.error(request, "Sorry! You can only review \
+    #                    products while signed in.")
+    #     return redirect(reverse('product_details', kwargs={'product_id':product_id}))
