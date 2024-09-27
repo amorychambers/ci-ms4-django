9aa97fe53988a5b309659c0254999d4ff8e8821a
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-27 18:48:07 +0100

    feat: add post method to create_post view

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import PostForm
+from .models import Post
 from products.models import Product
 
 # Create your views here.
@@ -10,7 +13,7 @@ def posts(request):
     """
     return render(request, 'posts/posts.html', )
 
-
+@login_required
 def create_post(request):
     """
     View to submit new community post
@@ -19,6 +22,22 @@ def create_post(request):
     form = PostForm()
     form.fields['product'].queryset = Product.objects.filter(
         tags__contains="coffee")
+    
+    if request.method == "POST":
+        post = Post.objects.create(
+            user=request.user,
+        )
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Check out your post in \
+                             the community tab!')
+            return redirect(reverse('posts'))
+        else:
+            messages.error(request, 'Something went wrong! Please check \
+                           that you included a title and content.')
+            return redirect(reverse('create_post',
+                                     kwargs={'form':form}))
 
     context = {
         "form": form,
--- products/models.py
+++ products/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=128)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=False)
     description = models.TextField()
     stock = models.IntegerField(default=0)
     tags = MultiSelectField(choices=TAG_CHOICES)
