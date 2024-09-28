45528c8b142d3e067300b3524e4249d0ea132908
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-28 21:46:09 +0100

    feat: add new contact.html with view and url

--- home/views.py
+++ home/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     """
-    View returns index homepage
+    View to return homepage
     """
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """
+    View to return and handle contact form,
+    with about us info
+    """
+
+    return render(request, 'home/contact.html')
