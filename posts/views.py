8507c49300a99316e7dc9c68537e37659ef5822b
Amory <108595086+amorychambers@users.noreply.github.com>
2024-09-28 12:36:29 +0100

    feat: add pagination to community posts page

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.contrib import messages
 from .forms import PostForm
 from .models import Post
+
+import math
 
 from products.models import Product
 
@@ -13,7 +16,19 @@ def posts(request):
     """
     View to render all community posts
     """
-    return render(request, 'posts/posts.html')
+
+    # Paginator code snippet taken from django documentation
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 1)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "page_obj": page_obj
+    }
+
+    return render(request, 'posts/posts.html', context)
 
 @login_required
 def create_post(request):
