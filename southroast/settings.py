851cf466491de65178858ec5b84461ffd6d91655
Amory <108595086+amorychambers@users.noreply.github.com>
2024-10-10 00:15:00 +0100

    test: revert to previous commit as os.environ not suitable

--- southroast/settings.py
+++ southroast/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = env('DEVELOPMENT')
 
 ALLOWED_HOSTS = ['localhost', 'southroast-614a5422facc.herokuapp.com']
 
