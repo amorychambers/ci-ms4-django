e5a387f9df7d97ef8b59dc0cc4445c0d7de58aa3
Amory <108595086+amorychambers@users.noreply.github.com>
2024-10-10 23:42:39 +0100

    test: test deployment without STATIC_ROOT setting in dhango v4.2

--- southroast/settings.py
+++ southroast/settings.py
@@ -209,8 +209,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
