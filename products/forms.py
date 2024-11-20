31fcf8f077b47a5189344fb56f1ce3700d3d8381
Amory <108595086+amorychambers@users.noreply.github.com>
2024-11-20 22:55:05 +0000

    feat: add minimum values to stock and price for add_product form

--- products/forms.py
+++ products/forms.py
@@ -8,3 +8,12 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
 
     image = forms.ImageField(label='Image', required=True,)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add mininum value for product quantity
+        """
+        super().__init__(*args, **kwargs)
+
+        self.fields['price'].widget.attrs['min'] = 0
+        self.fields['stock'].widget.attrs['min'] = 0
